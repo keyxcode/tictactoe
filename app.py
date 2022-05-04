e67bbfc653f4bd1feb319addc6e2df2cf0b87066
keyxcode <keyxcode@gmail.com>
2022-05-04 18:12:45 +0700

    Implemented win/ draw logic

--- app.py
+++ app.py
@@ -24,11 +24,46 @@ def index():
 @app.route("/play/<int:row>/<int:col>")
 def play(row, col):
     
+    # Play a move
     session["board"][row][col] = session["turn"]
-    
+
+    # Winning Logic
+    for i in range(3):
+        for j in range(3):
+            if session["board"][i][0] == session["board"][i][1] == session["board"][i][2] != None or \
+               session["board"][0][j] == session["board"][1][j] == session["board"][2][j] != None or \
+               session["board"][0][0] == session["board"][1][1] == session["board"][2][2] != None or\
+               session["board"][2][2] == session["board"][1][1] == session["board"][0][0] != None:
+               return render_template("winner.html", game=session["board"], turn=session["turn"])
+
+    # Draw Logic
+    draw = True
+    for i in range(3):
+        for j in range(3):
+            if session["board"][i][j] == None:
+                draw = False
+                break
+            else:
+                continue
+    if draw == True:
+        return render_template("winner.html", game=session["board"])
+
+    # Switch turn
     if session["turn"] == "X":
         session["turn"] = "O"
     else:
         session["turn"] = "X"
-        
+             
+    return redirect("/")
+
+
+@app.route("/reset")
+def reset():
+
+    # Reset the game board
+    session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+    
+    # Reset turn
+    session["turn"] = "X"
+    
     return redirect("/")
